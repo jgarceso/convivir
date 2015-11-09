d3a97917067e3090120918b0b2b745ea92efab56
favilesm <fabiola.aviles.munoz@gmail.com>
2015-11-08 23:13:22 -0300

    configuracion de rutas

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,20 +52,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-/*
-| -------------------------------------------------------------------------
-| Sample REST API Routes
-| -------------------------------------------------------------------------
-*/
-$route['apiProducts']['get'] = 'apiProducts/index';
-$route['apiProducts/(:num)']['get'] = 'apiProducts/find/$1';
-$route['apiProducts']['post'] = 'apiProducts/index';
-$route['apiProducts/(:num)']['put'] = 'apiProducts/index/$1';
-$route['apiProducts/(:num)']['delete'] = 'apiProducts/index/$1';
-
-
-
-$route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
-$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
-
