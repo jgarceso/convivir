b1cdf83d79e525da872cbfbd50d9d2e881ae3fde
Juan Garces <jpyval@gmail.com>
2015-11-16 05:21:52 -0300

    bug menú salir

--- application/views/categorias.php
+++ application/views/categorias.php
@@ -10,16 +10,7 @@
                     <?php  require 'menu.php'; ?>
 		</ul>
 </div>
-		<div class="contenedor-user-info">
-			<div class="user-info">
-				<span class="user-icon"></span><?php echo $_SESSION["usuario"] ?>
-                                <?php
-                                echo '<a href="'.$this->config->site_url().'CerrarSesion" class="icon-salir" title="Cerrar Sesión"></a>';
-                                echo '<a href="'.$this->config->site_url().'Administracion" class="icon-inicio" title="Inicio"></a>'
-                                ?>
-			</div>
-			<div></div>
-		</div>
+		<?php require_once 'sesionSection.php'?>
 		<div class="main-content">
 			<div class="contenedor-blanco">
 			 <?php echo $output; ?>
