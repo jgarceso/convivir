94d362cfc52af2204a70b085444f1f3ccbb077e9
Juan Garces <jpyval@gmail.com>
2015-11-19 21:50:56 -0300

    Agregada authorization. Se quitan archivos que no se van a usar.

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
+/*
 $route['api/apiProducts']["get"] = 'apiProducts/index';
 $route['api/apiProducts/(:num)']["get"] = 'apiProducts/find/$1';
 $route['api/apiProducts']["post"] = 'apiProducts/index';
@@ -68,5 +68,5 @@ $route['api/apiCategorias/(:num)']["delete"] = 'apiCategorias/index/$1';
 $route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
 $route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
 
-
+*/
 
