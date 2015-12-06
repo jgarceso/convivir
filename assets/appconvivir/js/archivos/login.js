8d81b37816f530ffe20747cd1a081f9acf330fbf
favilesm <fabiola.aviles.munoz@gmail.com>
2015-12-06 03:33:53 -0300

    Afinando el código

--- assets/appconvivir/js/archivos/login.js
+++ assets/appconvivir/js/archivos/login.js
@@ -75,48 +75,7 @@ function IniciarSesion() {
 	});
 };
 
-function guardarPass() {
-     $aux = getUrlVars()["username"];
-     
-	$.ajax({
-		url : "Security/changePass",
-		type : 'POST',
-		dataType : 'json',
-		data : {
-			pass : $("#input-newPass").val(),
-                        validaPassActual:false,
-                        username:$aux 
-		},
-               
-		success : function(data) {
-			if (data.Correcto == false) {
-		   	     alert(data.Mensaje);
- 			}else{
-                            alert(data.Mensaje);
-                            window.location = data.Url;
-			}
-			
-		},
-		error : function(xhr, ajaxOptions, thrownError) {
-		}
-	});
-};
-
 var nav4 = window.Event ? true : false;
-function AceptaNumero(evt) {
-	var key = nav4 ? evt.which : evt.keyCode;
-	return (key <= 13 || (key >= 48 && key <= 57));
-};
 
-function getUrlVars()
-{
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
+
+
