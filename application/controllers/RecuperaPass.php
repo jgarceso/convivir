55efeecaa46d2404865b3c75d568ecabcbaf2149
favilesm <fabiola.aviles.munoz@gmail.com>
2015-12-10 01:28:24 -0300

    Se limpia codigo

--- application/controllers/RecuperaPass.php
+++ application/controllers/RecuperaPass.php
@@ -4,7 +4,6 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 require_once 'BaseController.php';
-require_once 'Encripter.php';
 
 class RecuperaPass extends BaseController {
 
@@ -18,30 +17,4 @@ class RecuperaPass extends BaseController {
         $this->set_js_files(array("archivos/cambiaContrasena.js"));
         $this->mostrar_pagina("recuperaPass");
     }
-    
-    public function test() {
-        $name = $_POST["prueba"];
-        $mensaje = $name;
-        $url = "";
-        $correcto = true;
-        $options="";
-        try{
-            $this->load->model('api_model');
-            $data["array_tipos"]=$this->api_model->get_Tipos();
-
-            foreach ($data["array_tipos"] as $row_tipo){
-                     $options .= '<option value="' . $row_tipo->IdTipo . '">' . $row_tipo->Nombre . '</option>';
-            }
-        }catch(Exception $e){
-            $mensaje="ocurrió un error al cargar los tipos asociados a las categorías.";
-        }
-        
-        
-        $obj = (object) array('Correcto' => $correcto, 'Url' => $url, 'Mensaje' => $mensaje, 'Opciones' => $options);
-        echo json_encode($obj);
-
-    }
-    
-      
-
 }
