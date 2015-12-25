c3c68f46809da54cad2f5e1181df6003ef6a2f54
Juan Garces <jpyval@gmail.com>
2015-12-25 02:56:04 -0300

    COMMIT RESPALDO

--- application/controllers/api/Producto.php
+++ application/controllers/api/Producto.php
@@ -8,9 +8,60 @@ class Producto extends REST_Controller{
     
     function __construct() {
         parent::__construct();
-        $this->load->model('api_model');
+        $this->load->model('data_model');
     }
 
+     public function index_post(){
+        echo 'index_post';
+         if(! $this->post('product')){
+             $this->response(array(
+              'status' => FALSE,
+              'message' => 'Debe enviar el producto'
+          ), REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+        
+        $product_id = $this->api_model->save($this->post("producto"));
+        
+        if(! is_null($product_id)){
+           $this->response($product_id, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        }else{
+             $this->response(array(
+              'status' => FALSE,
+              'message' => 'Ha ocurrido un error'
+          ), REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+    
+       public function buscar_post(){
+//         if(! $this->post('product')){
+//             $this->response(array(
+//              'status' => FALSE,
+//              'message' => 'Debe enviar el producto'
+//          ), REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+//        }
+        
+        $result = $this->data_model->buscar_productos($this->post("strBusqueda"),$this->post("opciones"),$this->post("limite"));
+        
+        $this->response($result, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+//        if(! is_null($product_id)){
+//           $this->response($product_id, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+//        }else{
+//             $this->response(array(
+//              'status' => FALSE,
+//              'message' => 'Ha ocurrido un error'
+//          ), REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+//        }
+      
+ // JSON EJEMPLO PARA POSTMAN
+//        {
+// "opciones":{"tipo":"1,2", "producto":"true", "categoria":"true", "empresa":"false"},
+// "limite":10,
+// "strBusqueda":"pan"
+//}
+    }
+    
+    
+    
     public function index_get(){
       $products = $this->api_model->get_Productos();
       if(! is_null($products))
@@ -60,26 +111,7 @@ class Producto extends REST_Controller{
         }
     }
     
-    public function index_post(){
-        echo 'index_post';
-         if(! $this->post('product')){
-             $this->response(array(
-              'status' => FALSE,
-              'message' => 'Debe enviar el producto'
-          ), REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
-        }
-        
-        $product_id = $this->api_model->save($this->post("producto"));
-        
-        if(! is_null($product_id)){
-           $this->response($product_id, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-        }else{
-             $this->response(array(
-              'status' => FALSE,
-              'message' => 'Ha ocurrido un error'
-          ), REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
-        }
-    }
+   
     public function index_delete($id){
         echo 'metodo delete';
         if(! $id){
--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -42,4 +42,57 @@ class Data_model  extends CI_Model  {
             
             return null;
 	}
+        
+        public function buscar_productos ($strBusqueda, $camposBusqueda, $limite){
+        
+        $sql = "SELECT * FROM (
+                                SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa
+                                FROM producto p
+                                  INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
+                                  INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
+                                  INNER JOIN subcategoriaproducto sc ON sc.IdSubcategoria = p.IdSubcategoria
+                                  INNER JOIN empresa e ON e.IdEmpresa = p.IdEmpresa
+                                WHERE t.IdTipo IN(".$camposBusqueda['tipo'].")
+                                AND ";
+        
+        if($camposBusqueda['producto'] == true){
+                                $sql = $sql."p.Descripcion LIKE '".$strBusqueda."%'" ;
+        }
+        $sql = $sql."          ) a
+                LIMIT ".$limite."";
+        
+//       $sql = "SELECT * FROM (
+//                                SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa
+//                                FROM producto p
+//                                  INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
+//                                  INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
+//                                  INNER JOIN subcategoriaproducto sc ON sc.IdSubcategoria = p.IdSubcategoria
+//                                  INNER JOIN empresa e ON e.IdEmpresa = p.IdEmpresa
+//                                WHERE t.IdTipo IN(".$camposBusqueda['tipo'].")
+//                                  AND p.Descripcion LIKE '".$strBusqueda."%' 
+//                                  OR c.Nombre LIKE 'pan%'
+//                                  OR sc.Nombre LIKE 'pan%'
+//                                  OR e.Nombre LIKE 'pan%'
+//
+//                                UNION
+//
+//                                SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa
+//                                FROM producto p
+//                                  INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
+//                                  INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
+//                                  INNER JOIN subcategoriaproducto sc ON sc.IdSubcategoria = p.IdSubcategoria
+//                                  INNER JOIN empresa e ON e.IdEmpresa = p.IdEmpresa
+//                                WHERE 1 = 1 
+//                                  AND p.Descripcion LIKE '%pan%'
+//                                  OR c.Nombre LIKE '%pan%'
+//                                  OR sc.Nombre LIKE '%pan%'
+//                                  OR e.Nombre LIKE 'pan%'
+//                            ) a
+//            LIMIT ".$limite."";
+        
+        var_dump($sql);
+        $query = $this->db->query($sql);
+        $result = $query->result_array();
+        return $result;
+    }
 }
