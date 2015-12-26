fdd10aedb7c583bc4c6d2c2a4f17f3bc37a2c556
Juan Garces <jpyval@gmail.com>
2015-12-26 02:11:35 -0300

    Método para buscar productos

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -1,98 +1,95 @@
 <?php
 
-class Data_model  extends CI_Model  {
+class Data_model extends CI_Model {
 
-	function __construct()
-    {
+    function __construct() {
         parent::__construct();
-		$this->load->database();
+        $this->load->database();
     }
-	
-	public function get_Tipos($id = null){
-              if(! is_null($id)){
-                $query = $this->db->select("*")->from("tipoproducto")->where("IdTipo",$id)->get();
-                if($query->num_rows()==1){
-                    return $query->row_array();
-                }
+
+    public function get_Tipos($id = null) {
+        if (!is_null($id)) {
+            $query = $this->db->select("*")->from("tipoproducto")->where("IdTipo", $id)->get();
+            if ($query->num_rows() == 1) {
+                return $query->row_array();
+            }
+            return null;
+        }
+        $query = $this->db->select("*")->from("tipoproducto")->get();
+
+        if ($query->num_rows() > 0) {
+            return $query->result_array();
+        }
+        return null;
+    }
+
+    public function get_CategoriasxTipo($id = null) {
+        if (!is_null($id)) {
+            $query = $this->db->select("*")->from("categoriaproducto")->where("IdTipo", $id)->get();
+            if ($query->num_rows() > 0) {
+                return $query->result_array();
+            } else {
                 return null;
             }
-             $query = $this->db->select("*")->from("tipoproducto")->get();
-             
-            if($query->num_rows()>0){
-                return $query->result_array();
-            }
-            return null;
-	}
-        
-        
-          public function get_CategoriasxTipo($id = null){
-            if(! is_null($id)){
-                $query = $this->db->select("*")->from("categoriaproducto")->where("IdTipo",$id)->get();
-                if($query->num_rows()>0){
-                    return $query->result_array();
-                }else{
-                      return null;
-                }
-            }
-             $query = $this->db->select("*")->from("categoriaproducto")->order_by("Nombre")->get();
-             
-            if($query->num_rows()>0){
-                return $query->result_array();
-            }
-            
-            return null;
-	}
-        
-        public function buscar_productos ($strBusqueda, $camposBusqueda, $limite){
-        
-        $sql = "SELECT * FROM (
-                                SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa
+        }
+        $query = $this->db->select("*")->from("categoriaproducto")->order_by("Nombre")->get();
+
+        if ($query->num_rows() > 0) {
+            return $query->result_array();
+        }
+
+        return null;
+    }
+
+    public function buscar_productos($strBusqueda, $camposBusqueda, $limite) {
+       
+        $likeInicio = "LIKE '" . $strBusqueda . "%'";
+        $likeNormal = "LIKE '%" . $strBusqueda . "%'";
+        $sqlComun = "SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa, p.IdEstadoCertificacion
                                 FROM producto p
                                   INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
                                   INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
                                   INNER JOIN subcategoriaproducto sc ON sc.IdSubcategoria = p.IdSubcategoria
                                   INNER JOIN empresa e ON e.IdEmpresa = p.IdEmpresa
-                                WHERE t.IdTipo IN(".$camposBusqueda['tipo'].")
-                                AND ";
+                                WHERE t.IdTipo IN(" . $camposBusqueda['tipo'] . ")";
+
+
+        $sql = "SELECT * FROM (
+                               ".$sqlComun;
+        $sql = $sql." ".$this->obtener_condiciones_sql_productos($camposBusqueda,$likeInicio);
+        $sql = $sql."\n UNION ";
+        $sql = $sql."\n ".$sqlComun;
+        $sql = $sql." ".$this->obtener_condiciones_sql_productos($camposBusqueda,$likeNormal);
+        $sql = $sql . "\n          ) a
+                ORDER BY a.IdEstadoCertificacion
+                LIMIT " . $limite . "";
         
-        if($camposBusqueda['producto'] == true){
-                                $sql = $sql."p.Descripcion LIKE '".$strBusqueda."%'" ;
-        }
-        $sql = $sql."          ) a
-                LIMIT ".$limite."";
-        
-//       $sql = "SELECT * FROM (
-//                                SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa
-//                                FROM producto p
-//                                  INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
-//                                  INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
-//                                  INNER JOIN subcategoriaproducto sc ON sc.IdSubcategoria = p.IdSubcategoria
-//                                  INNER JOIN empresa e ON e.IdEmpresa = p.IdEmpresa
-//                                WHERE t.IdTipo IN(".$camposBusqueda['tipo'].")
-//                                  AND p.Descripcion LIKE '".$strBusqueda."%' 
-//                                  OR c.Nombre LIKE 'pan%'
-//                                  OR sc.Nombre LIKE 'pan%'
-//                                  OR e.Nombre LIKE 'pan%'
-//
-//                                UNION
-//
-//                                SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa
-//                                FROM producto p
-//                                  INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
-//                                  INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
-//                                  INNER JOIN subcategoriaproducto sc ON sc.IdSubcategoria = p.IdSubcategoria
-//                                  INNER JOIN empresa e ON e.IdEmpresa = p.IdEmpresa
-//                                WHERE 1 = 1 
-//                                  AND p.Descripcion LIKE '%pan%'
-//                                  OR c.Nombre LIKE '%pan%'
-//                                  OR sc.Nombre LIKE '%pan%'
-//                                  OR e.Nombre LIKE 'pan%'
-//                            ) a
-//            LIMIT ".$limite."";
-        
-        var_dump($sql);
         $query = $this->db->query($sql);
         $result = $query->result_array();
         return $result;
+    }
+
+    private function obtener_condiciones_sql_productos($camposBusqueda, $likeStr) {
+        $sql = "";
+        $operadorLogico = "AND";
+        if ($camposBusqueda['producto'] == true) {
+            $sql = $sql . "\n " . $operadorLogico . " p.Descripcion ".$likeStr;
+            $operadorLogico = "OR";
+        }
+
+        if ($camposBusqueda['categoria'] == true) {
+            $sql = $sql . "\n " . $operadorLogico . " c.Nombre ".$likeStr;
+            $operadorLogico = "OR";
+        }
+
+        if ($camposBusqueda['subcategoria'] == true) {
+            $sql = $sql . "\n " . $operadorLogico . " sc.Nombre ".$likeStr;
+            $operadorLogico = "OR";
+        }
+
+        if ($camposBusqueda['empresa'] == true) {
+            $sql = $sql . "\n " . $operadorLogico . " e.Nombre ".$likeStr;
+        }
+        return $sql;
     }
 }
