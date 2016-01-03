b69ce48c4395ae707b867e4e09578bf37cef917a
Juan Garces <jpyval@gmail.com>
2016-01-03 02:55:02 -0300

    fixes

--- application/models/Data_model.php
+++ application/models/Data_model.php
@@ -42,10 +42,9 @@ class Data_model extends CI_Model {
     }
 
     public function buscar_productos($strBusqueda, $camposBusqueda, $limite) {
-       
         $likeInicio = "LIKE '" . $strBusqueda . "%'";
         $likeNormal = "LIKE '%" . $strBusqueda . "%'";
-        $sqlComun = "SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoría, sc.Nombre AS SubCategoría, e.Nombre as Empresa, p.IdEstadoCertificacion
+        $sqlComun = "SELECT p.Descripcion AS Producto, t.Nombre AS Tipo, c.Nombre AS Categoria, sc.Nombre AS SubCategoria, e.Nombre as Empresa, p.IdEstadoCertificacion
                                 FROM producto p
                                   INNER JOIN tipoproducto t ON p.IdTipo = t.IdTipo
                                   INNER JOIN categoriaproducto c ON c.IdCategoria = p.IdCategoria
@@ -63,7 +62,7 @@ class Data_model extends CI_Model {
         $sql = $sql . "\n          ) a
                 ORDER BY a.IdEstadoCertificacion
                 LIMIT " . $limite . "";
-        
+
         $query = $this->db->query($sql);
         $result = $query->result_array();
         return $result;
@@ -72,22 +71,22 @@ class Data_model extends CI_Model {
     private function obtener_condiciones_sql_productos($camposBusqueda, $likeStr) {
         $sql = "";
         $operadorLogico = "AND";
-        if ($camposBusqueda['producto'] == true) {
+        if ($camposBusqueda['producto'] === 'true') {
             $sql = $sql . "\n " . $operadorLogico . " p.Descripcion ".$likeStr;
             $operadorLogico = "OR";
         }
 
-        if ($camposBusqueda['categoria'] == true) {
+        if ($camposBusqueda['categoria'] === 'true') {
             $sql = $sql . "\n " . $operadorLogico . " c.Nombre ".$likeStr;
             $operadorLogico = "OR";
         }
 
-        if ($camposBusqueda['subcategoria'] == true) {
+        if ($camposBusqueda['subcategoria'] === 'true') {
             $sql = $sql . "\n " . $operadorLogico . " sc.Nombre ".$likeStr;
             $operadorLogico = "OR";
         }
 
-        if ($camposBusqueda['empresa'] == true) {
+        if ($camposBusqueda['empresa'] === 'true') {
             $sql = $sql . "\n " . $operadorLogico . " e.Nombre ".$likeStr;
         }
         return $sql;
