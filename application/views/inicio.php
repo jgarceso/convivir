c5f19f579e1d1b5ba5cfb3b1801ef6f14d433820
Juan Garces <jpyval@gmail.com>
2016-01-11 00:07:18 -0300

    agregada portada en sitio administración

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -14,10 +14,20 @@
 </div>
 		<?php require_once 'sesionSection.php'?>
 		<div class="main-content">
-			<div class="contenedor-blanco">
-                            <p>
-                                Usted se encuentra en el portal de administración.
+			<div class="contenedor-blanco">                  
+                            <h1 class="inicio-titulo">Portal de Administración de Productos Certificados</h1>
+                            <div style="width:40%;float:left;overflow: hidden;height: 80%;">
+                                <?php
+                                echo '<img style="width:100%;height:100%;" src="'.$this->config->site_url().$this->convivir->imagenes_path.'convivir-portada.png" />';
+                                ?>
+                            </div>
+                            <div style="width:50%;float:left;">
+                            <p style="line-height: 1.5;">
+                                Usted se encuentra en el portal de administración. Aquí podrá encontrar diversos mantenedores para poder registrar cambios en la información 
+                                relacionada a los productos certificados. Para mayor detalle respecto al proceso, consulte el manual de usuario que se puede descargar del siguiente enlace:
+                                <br><br><a href="#">Descargar Manual de Usuario</a>
                             </p>
+                            </div>
 			</div>
 		</div>
 	</body>
---
+++ sql scripts/ACTUALIZACIONESTADOCERTIFICACION.sql
@@ -0,0 +1,24 @@
+﻿
+/*
+ESTADO 2 = RENOVACION, ESTADO 3 = VENCIDA
+2 = 85, 3 = 41*/
+
+UPDATE producto
+SET IdEstadoCertificacion = 3
+where IdProducto >= 1157 AND IdProducto <= 1197;
+
+UPDATE producto
+SET IdEstadoCertificacion = 2
+where IdProducto >= 1093 AND IdProducto <= 1156;
+
+UPDATE producto
+SET IdEstadoCertificacion = 2
+where IdProducto >= 203 AND IdProducto <= 223;
+
+UPDATE estadocertificacion
+SET Nombre = 'En Renovación'
+WHERE IdEstadoCertificacion = 2;
+
+UPDATE estadocertificacion
+SET Nombre = 'Vencida'
+WHERE IdEstadoCertificacion = 3;
